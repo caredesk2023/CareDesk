488e1fa1b77727d773114cb79912a064c6c463c5
caredesk2023 <124086078+caredesk2023@users.noreply.github.com>
2024-11-27 12:47:19 +0100

    Add files via upload

---
+++ cs_customizations/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Personalizaciones | CS",
+    'summary': """""",
+    'description': """""",
+    'author': "CareDesk Soluciones",
+    'website': "https://caredesk.es",
+    'category': 'Productivity',
+    'application': True,
+    'version': '0.2',
+    'depends': ['sale'],
+    'data': [
+        'R/R1/views/r1_sale_report_templates.xml',
+    ],
+}
